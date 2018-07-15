1a8ebeb4c784a8d8fb152233c714cf58289e3c0b
Marta <mwisniewska@ospartner.pl>
2018-07-15 19:39:40 +0200

    PWA features template customized

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const modalDiv = document.getElementById('modal');
     const checkButton = document.getElementById('check-button');
     const browserInfo = document.getElementById('browser-navigator');
+
     let userAgent = navigator.userAgent;
+    pwaCounter = 0;
     if(userAgent && browserInfo) {
         browserInfo.innerText = userAgent;
     }
@@ -11,12 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
     if (checkButton) {
         checkButton.addEventListener('click', function () {
             if (modalDiv) {
-                console.log(modalDiv);
                 modalDiv.style.removeProperty('display');
             }
         });
     }
     checkServiceWorker();
+    setPwaCounter();
 
 });
 
@@ -24,6 +26,15 @@ function checkServiceWorker() {
     const label = document.getElementById('service-worker');
     if(label && navigator.serviceWorker !== undefined) {
         label.innerHTML = '<i class="fas fa-check"></i>';
+        this.pwaCounter++;
     }
 }
+
+function setPwaCounter() {
+    const counterSpan = document.getElementById('pwa-counter');
+    if(counterSpan) {
+        counterSpan.innerText = pwaCounter;
+    }
+}
+
 
