0230612d89f940f7cdc1daab0b5f77e36e826452
Marta <mwisniewska@ospartner.pl>
2018-07-15 23:01:39 +0200

    PWA features handled

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
     if(userAgent && browserInfo) {
         browserInfo.innerText = userAgent;
     }
-    console.log(checkButton);
 
     if (checkButton) {
         checkButton.addEventListener('click', function () {
@@ -18,13 +17,61 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
     checkServiceWorker();
-    setPwaCounter();
+    checkShare();
+    checkStorage();
+    checkFullscreen();
 
+    setPwaCounter();
 });
 
 function checkServiceWorker() {
     const label = document.getElementById('service-worker');
     if(label && navigator.serviceWorker !== undefined) {
+        label.innerHTML = '<i class="fas fa-check"></i>';
+        this.pwaCounter++;
+        checkBackgroundSync();
+        checkNotification();
+        checkPayment();
+    }
+}
+function checkBackgroundSync() {
+    const label = document.getElementById('background-sync');
+    if(label && 'SyncManager' in window !== false) {
+        label.innerHTML = '<i class="fas fa-check"></i>';
+        this.pwaCounter++;
+    }
+}
+function checkNotification() {
+    const label = document.getElementById('push-notification');
+    if(label && 'PushManager' in window !== false) {
+        label.innerHTML = '<i class="fas fa-check"></i>';
+        this.pwaCounter++;
+    }
+}
+function checkStorage() {
+    const label = document.getElementById('cache-storage');
+    if(label && 'Cache' in window) {
+        label.innerHTML = '<i class="fas fa-check"></i>';
+        this.pwaCounter++;
+    }
+}
+function checkPayment() {
+    const label = document.getElementById('payments');
+    if(label && window.PaymentRequest) {
+        label.innerHTML = '<i class="fas fa-check"></i>';
+        this.pwaCounter++;
+    }
+}
+function checkShare() {
+    const label = document.getElementById('payments');
+    if(label && navigator.share) {
+        label.innerHTML = '<i class="fas fa-check"></i>';
+        this.pwaCounter++;
+    }
+}
+function checkFullscreen() {
+    const label = document.getElementById('fullscreen');
+    if(label && (document.body.webkitRequestFullScreen || document.body.requestFullscreen)) {
         label.innerHTML = '<i class="fas fa-check"></i>';
         this.pwaCounter++;
     }
